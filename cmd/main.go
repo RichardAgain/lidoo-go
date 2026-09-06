0f6b6aaae44fdf2dea0cc9324e51b626585122cf
richardo <ricardohernande04@gmail.com>
2026-09-05 23:48:27 -0400

    fix: main.go syntax errors

--- cmd/main.go
+++ cmd/main.go
@@ -29,43 +29,33 @@ func main() {
 		return
 	}
 
-	var err error
 	switch os.Args[1] {
-		case "list":
-			err = docker.List(os.Args[2:])
-		case "init":
-			err = docker.Init(os.Args[2:])
-		case "update":
-			err = docker.Update(os.Args[2:])
-		case "drop":
-			err = docker.Drop(os.Args[2:])
-		case "up":
-			err = docker.Up(os.Args[2:])
-		case "stop":
-			err = docker.Stop(os.Args[2:])
-		case "restart":
-			err = docker.Restart(os.Args[2:])
-		case "remove":
-			err = docker.Remove(os.Args[2:])
-		case "run":
-			err = docker.Run(os.Args[2:], state)
-		case "recreate":
-			err = docker.Recreate(os.Args[2:], state)
-		case "stop":
-			err = docker.Stop(os.Args[2:])
-		case "restart":
-			err = docker.Restart(os.Args[2:])
-		case "remove":
-			err = docker.Remove(os.Args[2:])
-		case "addons":
-			err = addons.Run(os.Args[2:], state)
-		default:
-			if len(os.Args) >= 5 && os.Args[2] == "addons" {
-				err = addons.RunForContainer(os.Args[1:], state)
-			} else {
-				usage()
-				exitCode = 2
-			}
+	case "list":
+		err = docker.List(os.Args[2:])
+	case "init":
+		err = docker.Init(os.Args[2:])
+	case "update":
+		err = docker.Update(os.Args[2:])
+	case "drop":
+		err = docker.Drop(os.Args[2:])
+	case "run":
+		err = docker.Run(os.Args[2:], state)
+	case "recreate":
+		err = docker.Recreate(os.Args[2:], state)
+	case "stop":
+		err = docker.Stop(os.Args[2:])
+	case "restart":
+		err = docker.Restart(os.Args[2:])
+	case "remove":
+		err = docker.Remove(os.Args[2:])
+	case "addons":
+		err = addons.Run(os.Args[2:], state)
+	default:
+		if len(os.Args) >= 5 && os.Args[2] == "addons" {
+			err = addons.RunForContainer(os.Args[1:], state)
+		} else {
+			usage()
+			exitCode = 2
 		}
 	}
 
@@ -88,7 +78,7 @@ func usage() {
 	fmt.Fprintln(os.Stderr, "  init --name <profile> --database <database> [--modules <csv>]")
 	fmt.Fprintln(os.Stderr, "  update --name <profile> --database <database> [--update-all]")
 	fmt.Fprintln(os.Stderr, "  drop --name <profile> --database <database> --yes")
-	fmt.Fprintln(os.Stderr, "  up|stop|restart|remove --name <profile>")
+	fmt.Fprintln(os.Stderr, "  run|stop|restart|remove --name <profile>")
 	fmt.Fprintln(os.Stderr, "       lidoo addons add <addon name> <git url>")
 	fmt.Fprintln(os.Stderr, "       lidoo <container> addons add <addon name> [<addon name> ...]")
 }
